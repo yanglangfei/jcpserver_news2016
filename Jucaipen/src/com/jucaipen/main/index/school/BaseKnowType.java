31f7bb8c80f3f2e8d4ac3c0e0d49df989fd4fffc
langfei yang <185601452@qq.com>
2016-06-12 18:37:15 +0800

    lst

--- Jucaipen/src/com/jucaipen/main/index/school/BaseKnowType.java
+++ Jucaipen/src/com/jucaipen/main/index/school/BaseKnowType.java
@@ -15,38 +15,50 @@ import com.jucaipen.utils.JsonUtil;
 import com.jucaipen.utils.StringUtil;
 
 /**
- * @author Administrator
- *      获取学院基础知识 分类信息   getbasetype
- *      
+ * @author Administrator 获取学院基础知识 分类信息 getbasetype
+ * 
  */
 @SuppressWarnings("serial")
 public class BaseKnowType extends HttpServlet {
 	private String result;
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.setCharacterEncoding("UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		String bigId=request.getParameter("bigId");
-		if(StringUtil.isNotNull(bigId)){
-			if(StringUtil.isInteger(bigId)){
-				int bId=Integer.parseInt(bigId);
-				result=initBaseType(bId);
-			}else{
-				result=JsonUtil.getRetMsg(2,"bigId 参数数字格式化异常");
+		String leavel = request.getParameter("leavel");
+		String bigId = request.getParameter("bigId");
+		if (StringUtil.isNotNull(bigId)) {
+			if (StringUtil.isInteger(bigId)) {
+				int bId = Integer.parseInt(bigId);
+				if (StringUtil.isNotNull(leavel)
+						&& StringUtil.isInteger(leavel)) {
+					int lId = Integer.parseInt(leavel);
+					result = initBaseType(bId, lId);
+				}else{
+					result=JsonUtil.getRetMsg(3,"leavel 参数异常");
+				}
+			} else {
+				result = JsonUtil.getRetMsg(2, "bigId 参数数字格式化异常");
 			}
-		}else{
-			result=JsonUtil.getRetMsg(1,"bigId 参数不能为空");
+		} else {
+			result = JsonUtil.getRetMsg(1, "bigId 参数不能为空");
 		}
 		out.println(result);
 		out.flush();
 		out.close();
 	}
-	private String initBaseType(int pId) {
-		//初始化学院分类信息
-		List<KnowledgeClass> knowTypes = KnowledgeClassSer.findKnowClassByPid(pId);
-		return JsonUtil.getKnowType(knowTypes);
+
+	private String initBaseType(int pId, int lId) {
+		// 初始化学院分类信息
+		List<KnowledgeClass> knowTypes = KnowledgeClassSer
+				.findKnowClassByPid(pId);
+		if (lId == 0) {
+			return JsonUtil.getSingleKnowType(knowTypes);
+		}
+		return JsonUtil.getMoreKnowType(knowTypes);
 	}
 
 }
