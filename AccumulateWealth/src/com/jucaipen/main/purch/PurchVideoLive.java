649676257b810af1959fddc50fa478dbe244e334
langfei yang <185601452@qq.com>
2016-09-30 10:17:55 +0800

    n

---
+++ AccumulateWealth/src/com/jucaipen/main/purch/PurchVideoLive.java
@@ -0,0 +1,193 @@
+package com.jucaipen.main.purch;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.jucaipen.main.datautils.RollBackUtil;
+import com.jucaipen.model.Account;
+import com.jucaipen.model.AccountDetail;
+import com.jucaipen.model.Contribute;
+import com.jucaipen.model.FamousTeacher;
+import com.jucaipen.model.Rebate;
+import com.jucaipen.model.SysAccount;
+import com.jucaipen.model.SysDetailAccount;
+import com.jucaipen.model.User;
+import com.jucaipen.model.VideoLive;
+import com.jucaipen.model.VideoLiveSale;
+import com.jucaipen.service.AccountSer;
+import com.jucaipen.service.FamousTeacherSer;
+import com.jucaipen.service.SysAccountSer;
+import com.jucaipen.service.UserServer;
+import com.jucaipen.service.VideoLiveSaleSer;
+import com.jucaipen.service.VideoLiveServer;
+import com.jucaipen.utils.JsonUtil;
+import com.jucaipen.utils.StringUtil;
+import com.jucaipen.utils.TimeUtils;
+
+/**
+ * @author Administrator
+ *
+ *   购买视频直播
+ */
+public class PurchVideoLive extends HttpServlet {
+	private static final long serialVersionUID = -5454633355323857858L;
+	private String result;
+	private String ip;
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userId = request.getParameter("userId");
+		String videoId = request.getParameter("liveId");
+		String bills = request.getParameter("bills");
+		String days = request.getParameter("days");
+		ip=request.getRemoteAddr();
+		if(StringUtil.isNotNull(userId)&&StringUtil.isInteger(userId)){
+			int uId=Integer.parseInt(userId);
+			if(StringUtil.isNotNull(videoId)&&StringUtil.isInteger(videoId)){
+				int vId=Integer.parseInt(videoId);
+				if(StringUtil.isNotNull(bills)&&StringUtil.isInteger(bills)){
+					int b=Integer.parseInt(bills);
+					if(StringUtil.isNotNull(days)&&StringUtil.isInteger(days)){
+						int d=Integer.parseInt(days);
+						result=purchVideoLive(uId,d,vId,b);
+					}else{
+						result=JsonUtil.getRetMsg(4,"days 参数异常");
+					}
+				}else{
+					result=JsonUtil.getRetMsg(3,"bills 参数异常");
+				}
+				
+			}else{
+				result=JsonUtil.getRetMsg(2, "videoId 参数异常");
+			}
+		}else{
+			result=JsonUtil.getRetMsg(1, "userId 参数异常");
+		}
+		out.print(result);
+		out.flush();
+		out.close();
+	}
+
+	private String purchVideoLive(int uId, int d, int vId, int b) {
+		Account account=AccountSer.findAccountByUserId(uId);
+		if(b<=0){
+			return JsonUtil.getRetMsg(1,"该直播暂不支持购买");
+		}
+		
+		if(account==null||account.getJucaiBills()<b){
+			return JsonUtil.getRetMsg(2,"聚财币不足，请进行充值");
+		}
+		
+		VideoLiveSale liveSale = VideoLiveSaleSer.findSaleByUidAndLiveId(uId, vId);
+		if(liveSale!=null){
+			if(TimeUtils.isBetwent(liveSale.getStartDate(), liveSale.getEndDate())){
+				return JsonUtil.getRetMsg(3, "该直播已经购买");
+			}
+		}
+		
+		User user=UserServer.findBaseInfoById(uId);
+		
+		VideoLive live = VideoLiveServer.getRoomInfo(vId);
+		int teacherId = live.getTeacherId();
+		FamousTeacher teacher=FamousTeacherSer.findFamousTeacherById(teacherId);
+		
+		
+		String startDate = TimeUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
+		String endDate = TimeUtils.format(TimeUtils.addBaseDay(new Date(), d),
+				"yyyy-MM-dd HH:mm:ss");
+		
+		
+		AccountDetail detail = new AccountDetail();
+		detail.setDetailMoney(b);
+		detail.setDetailType(1);
+		detail.setInsertDate(TimeUtils
+				.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+		detail.setIsDel(0);
+		detail.setOrderCode("");
+		detail.setRemark("购买【"+teacher.getNickName()+"】的直播《"+live.getTitle()+"》");
+		detail.setState(0);
+		detail.setUserId(uId);
+		
+		
+		AccountDetail integeralDetail = new AccountDetail();
+		integeralDetail.setDetailMoney(b);
+		integeralDetail.setDetailType(0);
+		integeralDetail.setInsertDate(TimeUtils
+				.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+		integeralDetail.setIsDel(0);
+		integeralDetail.setOrderCode("");
+		integeralDetail.setRemark("购买【"+teacher.getNickName()+"】的直播《"+live.getTitle()+"》,账户积分+"+b);
+		integeralDetail.setState(1);
+		integeralDetail.setUserId(uId);
+		
+		
+		VideoLiveSale sale=new VideoLiveSale();
+		sale.setEndDate(endDate);
+		sale.setFk_liveId(vId);
+		sale.setInsertDate(TimeUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+		sale.setIsStop(0);
+		sale.setOrderCode("");
+		sale.setStartDate(startDate);
+		sale.setTeacherId(teacherId);
+		sale.setUserId(uId);
+		
+		
+		Contribute contribute = new Contribute();
+		contribute.setAllJucaiBills(b);
+		contribute.setComType(11);
+		contribute.setFk_id(vId);
+		contribute.setTeacherId(teacherId);
+		contribute.setUserId(uId);
+		contribute.setInsertDate(TimeUtils.format(new Date(),
+				"yyyy-MM-dd HH:mm:ss"));
+		
+		
+		SysAccount sysAccount=SysAccountSer.findAccountInfo();
+		
+		
+		SysDetailAccount detailAccount=new SysDetailAccount();
+		detailAccount.setInsertDate(TimeUtils.format(new Date(),
+				"yyyy-MM-dd HH:mm:ss"));
+		detailAccount.setIp(ip);
+		detailAccount.setIsDel(0);
+		detailAccount.setOrderId(0);
+		detailAccount.setPrice(b);
+		detailAccount.setRecoderType(2);
+		detailAccount.setRemark("购买【"+teacher.getNickName()+"】的直播《"+live.getTitle()+"》");
+		detailAccount.setType(5);
+		detailAccount.setUserId(uId);
+		
+		Rebate rebate=new Rebate();
+		rebate.setTeacherId(teacherId);
+		// 返利类型（0讲师返利记录，1系统收入记录）
+		rebate.setType(0);
+		rebate.setRebateMoney(b * teacher.getReturnRate());
+		rebate.setFromId(uId);
+		rebate.setInsertDate(TimeUtils
+				.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+		rebate.setRemark("用户购买直播返利");
+		
+		
+		Rebate sysRebate=new Rebate();
+		rebate.setTeacherId(teacherId);
+		// 返利类型（0讲师返利记录，1系统收入记录）
+		rebate.setType(1);
+		rebate.setRebateMoney(b * (1-teacher.getReturnRate()));
+		rebate.setFromId(uId);
+		rebate.setInsertDate(TimeUtils
+				.format(new Date(), "yyyy-MM-dd HH:mm:ss"));
+		rebate.setRemark("用户购买直播返利");
+		
+		int isSuccess=RollBackUtil.purchLiveVideo(sale,detail,integeralDetail,b,account,uId,user,contribute,sysAccount,detailAccount,rebate,sysRebate);
+		return isSuccess==1 ? JsonUtil.getRetMsg(0, "购买成功") : JsonUtil.getRetMsg(1, "购买失败");
+	}
+
+}
---
+++ AccumulateWealth/src/com/jucaipen/model/StudioMsg.java
@@ -0,0 +1,199 @@
+package com.jucaipen.model;
+
+import java.io.Serializable;
+
+/**
+ * @author Administrator  ------------新增数据
+ *
+ *  演播室消息  
+ */
+@SuppressWarnings("serial")
+public class StudioMsg implements Serializable{
+	/**
+	 *  Id
+	 */
+	private int id;
+	/**
+	 *  SendUserId
+	 */
+	private int sendUserId;
+	/**
+	 *  SendName
+	 */
+	private String sendName;
+	
+	private String sendFace;
+	/**
+	 *  Msg
+	 */
+	private String msg;
+	/**
+	 *  shenhe
+	 */
+	private int shenhe;
+	/**
+	 *  ReceiverId
+	 */
+	private int receiverId;
+	/**
+	 *  ReceiverName
+	 */
+	private String receiverName;
+	
+	private String receiverFace;
+	/**
+	 *  Fk_VideoLiveId
+	 */
+	private int videoLiveId;
+	/**
+	 *  BuyProductId  守护等级
+	 */
+	private int buyProductId;
+	/**
+	 *  IsSysAdmin
+	 */
+	private int isSysAdmin;
+	/**
+	 *  IsRoomAdmin
+	 */
+	private int isRoomAdmin;
+	/**
+	 *  SendDate
+	 */
+	private String sendDate;
+	private int isTeacher;
+	private int isShouhu;
+	private int isChatAdmin;
+	private int userLeavel;
+	private int isServer;
+	
+	/**
+	 *  IP
+	 */
+	private String ip;
+	
+	
+	
+	
+	
+	public String getSendFace() {
+		return sendFace;
+	}
+	public void setSendFace(String sendFace) {
+		this.sendFace = sendFace;
+	}
+	public String getReceiverFace() {
+		return receiverFace;
+	}
+	public void setReceiverFace(String receiverFace) {
+		this.receiverFace = receiverFace;
+	}
+	public int getIsServer() {
+		return isServer;
+	}
+	public void setIsServer(int isServer) {
+		this.isServer = isServer;
+	}
+	public int getUserLeavel() {
+		return userLeavel;
+	}
+	public void setUserLeavel(int userLeavel) {
+		this.userLeavel = userLeavel;
+	}
+	public int getIsTeacher() {
+		return isTeacher;
+	}
+	public void setIsTeacher(int isTeacher) {
+		this.isTeacher = isTeacher;
+	}
+	public int getIsShouhu() {
+		return isShouhu;
+	}
+	public void setIsShouhu(int isShouhu) {
+		this.isShouhu = isShouhu;
+	}
+	public int getIsChatAdmin() {
+		return isChatAdmin;
+	}
+	public void setIsChatAdmin(int isChatAdmin) {
+		this.isChatAdmin = isChatAdmin;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getSendUserId() {
+		return sendUserId;
+	}
+	public void setSendUserId(int sendUserId) {
+		this.sendUserId = sendUserId;
+	}
+	public String getSendName() {
+		return sendName;
+	}
+	public void setSendName(String sendName) {
+		this.sendName = sendName;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public int getShenhe() {
+		return shenhe;
+	}
+	public void setShenhe(int shenhe) {
+		this.shenhe = shenhe;
+	}
+	public int getReceiverId() {
+		return receiverId;
+	}
+	public void setReceiverId(int receiverId) {
+		this.receiverId = receiverId;
+	}
+	public String getReceiverName() {
+		return receiverName;
+	}
+	public void setReceiverName(String receiverName) {
+		this.receiverName = receiverName;
+	}
+	public int getVideoLiveId() {
+		return videoLiveId;
+	}
+	public void setVideoLiveId(int videoLiveId) {
+		this.videoLiveId = videoLiveId;
+	}
+	public int getBuyProductId() {
+		return buyProductId;
+	}
+	public void setBuyProductId(int buyProductId) {
+		this.buyProductId = buyProductId;
+	}
+	public int getIsSysAdmin() {
+		return isSysAdmin;
+	}
+	public void setIsSysAdmin(int isSysAdmin) {
+		this.isSysAdmin = isSysAdmin;
+	}
+	public int getIsRoomAdmin() {
+		return isRoomAdmin;
+	}
+	public void setIsRoomAdmin(int isRoomAdmin) {
+		this.isRoomAdmin = isRoomAdmin;
+	}
+	public String getSendDate() {
+		return sendDate;
+	}
+	public void setSendDate(String sendDate) {
+		this.sendDate = sendDate;
+	}
+	public String getIp() {
+		return ip;
+	}
+	public void setIp(String ip) {
+		this.ip = ip;
+	}
+}
